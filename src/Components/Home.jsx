793e0dbcb798dfa41e35c3d2277afae84fcb4d50
ankita-joshi2810 <ankitajoshiash2810@gmail.com>
2023-10-03 23:15:06 +0530

    adding cards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Carousel, Row, Col } from 'react-bootstrap'
+import { Carousel, Row, Col, Container, Card, Button } from 'react-bootstrap'
 import slide1 from '../Assets/slide01.jpg'
 import slide2 from '../Assets/slide02.jpg'
 import slide3 from '../Assets/slide03.jpg'
+import card1 from '../Assets/north-indian.jpg'
+import card2 from '../Assets/south-indian.jpg'
+import card3 from '../Assets/chinese.jpg'
+import card4 from '../Assets/pizza.jpg'
 
 const Home = () => {
     return (
@@ -35,7 +39,65 @@ const Home = () => {
                 </Col>
             </Row>
             <Row>
-                <Col>1 of 1</Col>
+                <Col><Container>
+                    <Row>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={card1} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={card2} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={card3} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={card4} />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                </Container></Col>
             </Row>
         </div>
     )
